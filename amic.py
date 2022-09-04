741e3096927943fa24fdddd136cd0b28d3737d7e
tybiswal <kanhatheman@gmail.com>
2022-09-04 13:34:28 +0630

    Created a projects directory folder

--- amic.py
+++
@@ -1,8 +0,0 @@
-# Have you made a new app soon?
-# Appending to lists
-
-a_list = []
-
-for i in range(0,3):
-    a_list.append(input("Type a word: "))
-    print(a_list)
--- requiem.py
+++
@@ -1,5 +0,0 @@
-import requests as r
-
-r.get('https://api.github.com/events')
-
-print(r)
